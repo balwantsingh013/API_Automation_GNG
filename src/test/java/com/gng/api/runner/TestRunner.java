caed45c667cfbb233bc02bab41091265f4105eb5
Pagar <Krutika.Pagar@vertexone.net>
2025-02-18 19:59:56 +0530

    uncomment tag

--- src/test/java/com/gng/api/runner/TestRunner.java
+++ src/test/java/com/gng/api/runner/TestRunner.java
@@ -23,7 +23,7 @@ import static com.gng.api.context.ApplicationContext.setRequestSpec;
         glue = {"com.gng.api.steps"},
         dryRun = false,
         monochrome = true,
-       tags = "@SearchAccountsSSPBasedOnTheProvidedWithFirstNameAndLastNameAndZipAndEType",
+       //tags = "@SearchAccountsSSPBasedOnTheProvidedWithFirstNameAndLastNameAndZipAndEType",
         plugin = {
                 "pretty",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
