23d547a428269214a00df69a46de5fbcf700d5bb
Jadhav <Kajal.Jadhav@vertexone.net>
2025-06-20 14:05:33 +0000

    code changes for get reasons for leaving

--- src/test/java/com/gng/api/pages/turnOff/CommonPages/GetReasonsForLeavingPage/GetReasonsForLeavingApiPage.java
+++ src/test/java/com/gng/api/pages/turnOff/CommonPages/GetReasonsForLeavingPage/GetReasonsForLeavingApiPage.java
@@ -1,10 +1,31 @@
 package com.gng.api.pages.turnOff.CommonPages.GetReasonsForLeavingPage;
 
+import com.gng.api.pages.BasePage;
+import com.gng.api.pojo.CommonPojo.GetReasonsForLeaving.GetReasonsForLeavingRequest;
+import com.gng.api.pojo.TestContext.TestContext;
 import com.gng.api.steps.turnOff.Common.GetReasonsForLeaving.GetReasonsForLeavingLabel;
+import io.restassured.response.Response;
+import org.apache.http.client.methods.HttpPost;
 
-public class GetReasonsForLeavingApiPage {
+import static com.gng.api.constants.ApiEndPoint.SAVE_UNENROLLMENT;
+
+public class GetReasonsForLeavingApiPage extends BasePage {
+
+    private final GetReasonsForLeavingHelper helper;
+
+    public GetReasonsForLeavingApiPage(TestContext testContext) {
+        super(testContext);
+        this.helper = new GetReasonsForLeavingHelper(testContext);
+    }
 
     public void requestToGetReasonsForLeavingWithETCFlag(GetReasonsForLeavingLabel apiLabel, boolean etcExists){
+
+        GetReasonsForLeavingRequest payload = helper.preparePayload(apiLabel);
+        helper.setEtcExistsAndRequestID(payload,etcExists);
+
+        setRequestSpecification(payload, testContext.getAuthToken());
+        Response response = sendRequest(HttpPost.METHOD_NAME, SAVE_UNENROLLMENT, 200);
+        testContext.setResponse(response);
 
     }
 }
