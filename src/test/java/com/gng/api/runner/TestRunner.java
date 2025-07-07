f49a863f8b9523e67acc8cbebb0720a0f66afb51
balwantsingh013 <balwantsingh0892@gmail.com>
2025-07-07 19:09:05 +0530

    extent report enhancements

--- src/test/java/com/gng/api/runner/TestRunner.java
+++ src/test/java/com/gng/api/runner/TestRunner.java
@@ -25,7 +25,7 @@ import static com.gng.api.context.ApplicationContext.setRequestSpec;
         glue = {"com.gng.api.steps"},
         dryRun = false,
         monochrome = true,
-        //tags = "@GetUserRolesInvalidPassword",
+        //tags = "@validAGLCAccountNumberRSActiveAccount",
 
         plugin = {
                 "pretty",
