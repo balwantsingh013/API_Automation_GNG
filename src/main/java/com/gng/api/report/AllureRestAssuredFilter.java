a71c81007e60e82b8e191e2cf6a4693a93ae3d52
Jadhav <Kajal.Jadhav@vertexone.net>
2025-09-25 15:48:33 +0000

    test

--- src/main/java/com/gng/api/report/AllureRestAssuredFilter.java
+++ src/main/java/com/gng/api/report/AllureRestAssuredFilter.java
@@ -14,18 +14,17 @@ public class AllureRestAssuredFilter implements Filter {
 
     @Override
     public Response filter(FilterableRequestSpecification requestSpec, FilterableResponseSpecification responseSpec, FilterContext context) {
-        // Capture request details (headers removed) only if not auth token
+        // Capture request body only if not auth token
         if (!requestSpec.getURI().contains("/Authentication/GetToken")) {
-            String requestBuilder = "Request URI: " + requestSpec.getURI() + "\n" +
-                    "Request Method: " + requestSpec.getMethod() + "\n" +
-                    "Request Body: " + (requestSpec.getBody() != null ? requestSpec.getBody().toString() : "No Body");
+            String requestBuilder = "Request Body: " +
+                    (requestSpec.getBody() != null ? requestSpec.getBody().toString() : "No Body");
             Allure.addAttachment("Request", new ByteArrayInputStream(requestBuilder.getBytes(StandardCharsets.UTF_8)));
         }
 
         // Proceed with the request
         Response response = context.next(requestSpec, responseSpec);
 
-        // Capture response details (headers removed) only if not auth token
+        // Capture response details only if not auth token
         if (!requestSpec.getURI().contains("/Authentication/GetToken")) {
             String responseBuilder = "Response Status Code: " + response.getStatusCode() + "\n" +
                     "Response Body: " + response.getBody().asString();
