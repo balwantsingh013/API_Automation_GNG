0001a76f3b5a6bad4824de619301ebc74a356d89
jgassner77 <judson.gassner@vertexone.net>
2025-10-21 18:45:45 -0500

    GetEligiblePlansAndOffers service Transfer

--- src/test/java/com/gng/api/runner/TestRunner.java
+++ src/test/java/com/gng/api/runner/TestRunner.java
@@ -20,12 +20,12 @@ import static com.gng.api.context.ApplicationContext.setRequestSpec;
 
 @CucumberOptions(
         features = {
-                "src/test/resources/features/phase1/serviceTransfer"
+                "src/test/resources/features/phase1/turnOff"
         },
         glue = {"com.gng.api.steps"},
         dryRun = false,
         monochrome = true,
-        tags = "@GetEligiblePlansAndOffersTransferWithSearchAccountsNotAllowed",
+        //tags = "@HappyFlow",
         plugin = {
                 "pretty",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
