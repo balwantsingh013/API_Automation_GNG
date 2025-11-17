3aef10fc8bea029f7e884fdcafcc41864776dfe9
jgassner77 <judson.gassner@vertexone.net>
2025-11-17 14:35:21 -0600

    get default meterSet

---
+++ src/test/java/com/gng/api/steps/meterSet/ServiceOrdersSteps/GetDefaultPlansAndOffers/GetDefaultPlansAndOffersApiLabel.java
@@ -0,0 +1,12 @@
+package com.gng.api.steps.meterSet.ServiceOrdersSteps.GetDefaultPlansAndOffers;
+
+public enum GetDefaultPlansAndOffersApiLabel {
+    //type
+    get_default_plans_and_offers,
+    get_default_plans_and_offers_mandatory,
+
+    GD_MS_MISSING_TRANSACTION_ID_TC_03,
+    GD_MS_INVALID_TRANSACTION_ID_LENGTH_TC_04,
+    GD_MS_MISSING_TRANSACTION_ID_TC_05
+
+}
